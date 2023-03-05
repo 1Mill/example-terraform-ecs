df94ab13c8b2d1aa781d5da7f468b5b8e3338dda
1Mill <erik.ekberg@outlook.com>
2023-03-04 21:34:07 -0800

    Abstract ecr_address into local var

--- examples/aws-resources/main.tf
+++ examples/aws-resources/main.tf
@@ -14,13 +14,12 @@ provider "aws" {
 data "aws_caller_identity" "this" {}
 data "aws_ecr_authorization_token" "this" {}
 data "aws_region" "this" {}
+locals { ecr_address = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.this.name) }
 provider "docker" {
 	registry_auth {
-		address  = format("%v.dkr.ecr.%v.amazonaws.com", data.aws_caller_identity.this.account_id, data.aws_region.this.name)
+		address  = local.ecr_address
 		password = data.aws_ecr_authorization_token.this.password
 		username = data.aws_ecr_authorization_token.this.user_name
-	}
-}
 	}
 }
 
