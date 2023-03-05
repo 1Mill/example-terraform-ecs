52417f515a59b7e9a01f80756bd64a37411dd38a
1Mill <erik.ekberg@outlook.com>
2023-03-05 15:09:13 -0800

    Add datetime to hello world output

--- examples/aws-resources/index.js
+++ examples/aws-resources/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (_req, res) => {
-	res.send(`Hello World! The NODE_ENV is ${process.env.NODE_ENV}.`)
+	res.send(`Hello World! The NODE_ENV is ${process.env.NODE_ENV}. Datetime now: ${new Date().toISOString()}`)
 })
 
 app.listen(process.env.PORT, () => {
