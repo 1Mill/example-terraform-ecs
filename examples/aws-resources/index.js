0ac247e471026972109c88058547c5568c97e269
1Mill <erik.ekberg@outlook.com>
2023-03-05 20:03:08 -0800

    Add and deploy MY_INPUT_ENV_VAR

--- examples/aws-resources/index.js
+++ examples/aws-resources/index.js
@@ -3,7 +3,12 @@ const express = require('express')
 const app = express()
 
 app.get('/', (_req, res) => {
-	res.send(`Hello World! The NODE_ENV is ${process.env.NODE_ENV}. Datetime now: ${new Date().toISOString()}`)
+	res.send(`
+		Hello World!
+		The NODE_ENV is ${process.env.NODE_ENV}.
+		Datetime now: ${new Date().toISOString()}.
+		The MY_INPUT_ENV_VAR is ${process.env.MY_INPUT_ENV_VAR}.
+	`)
 })
 
 app.listen(process.env.PORT, () => {
