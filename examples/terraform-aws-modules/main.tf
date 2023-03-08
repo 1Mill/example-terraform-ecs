83f20c52d1b323dcb2bb0a2138aa51127d19d954
1Mill <erik.ekberg@outlook.com>
2023-03-08 15:20:29 -0800

    Create VPC

--- examples/terraform-aws-modules/main.tf
+++ examples/terraform-aws-modules/main.tf
@@ -59,3 +59,17 @@ resource "docker_registry_image" "this" {
 	keep_remotely = true # Do not delete the old image when a new image is built
 	name = resource.docker_image.this.name
 }
+
+# * Part 3 - Create VPC
+data "aws_availability_zones" "available" { state = "available" }
+module "vpc" {
+	source = "terraform-aws-modules/vpc/aws"
+	version = "~> 3.19.0"
+
+	azs = slice(data.aws_availability_zones.available.names, 0, 2) # Span subnetworks across multiple avalibility zones
+	cidr = "10.0.0.0/16"
+	create_igw = true # Expose public subnetworks to the Internet
+	enable_nat_gateway = true # Hide private subnetworks behind NAT Gateway
+	private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+	public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+}
