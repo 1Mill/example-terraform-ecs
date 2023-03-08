dfdca3b76b3d98d3c08b07013deabcecb71dd088
1Mill <erik.ekberg@outlook.com>
2023-03-08 15:53:45 -0800

    Create application load balancer

--- examples/terraform-aws-modules/main.tf
+++ examples/terraform-aws-modules/main.tf
@@ -73,3 +73,62 @@ module "vpc" {
 	private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
 	public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 }
+
+# * Part 4 - Create application load balancer
+module "alb" {
+	source  = "terraform-aws-modules/alb/aws"
+	version = "~> 8.4.0"
+
+	load_balancer_type = "application"
+	security_groups = [module.vpc.default_security_group_id]
+	subnets = module.vpc.public_subnets
+	vpc_id = module.vpc.vpc_id
+
+	security_group_rules = {
+		ingress_all_http = {
+			type        = "ingress"
+			from_port   = 80
+			to_port     = 80
+			protocol    = "TCP"
+			description = "HTTP web traffic"
+			cidr_blocks = ["0.0.0.0/0"]
+		}
+		ingress_hello_world = {
+			type        = "ingress"
+			from_port   = local.container_port
+			to_port     = local.container_port
+			protocol    = "TCP"
+			description = "Hello World Application"
+			cidr_blocks = ["0.0.0.0/0"]
+		}
+		egress_all = {
+			type        = "egress"
+			from_port   = 0
+			to_port     = 0
+			protocol    = "-1"
+			cidr_blocks = ["0.0.0.0/0"]
+		}
+	}
+
+	http_tcp_listeners = [
+		{
+			port               = 80
+			protocol           = "HTTP"
+			target_group_index = 0
+		}
+	]
+
+	target_groups = [
+		{
+			backend_port         = local.container_port
+			backend_protocol     = "HTTP"
+			target_type          = "ip"
+		}
+	]
+}
+
+# TODO: Update Step number
+# * Step XXX - See our application working.
+# * Output the URL of our Application Load Balancer so that we can connect to
+# * our application running inside  ECS once it is up and running.
+output "lb_url" { value = "http://${module.alb.lb_dns_name}" }
