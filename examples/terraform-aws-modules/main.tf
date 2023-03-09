d61fcbf60f21ca57812cf98a9239e989f975670c
1Mill <erik.ekberg@outlook.com>
2023-03-08 16:03:35 -0800

    Create task def

--- examples/terraform-aws-modules/main.tf
+++ examples/terraform-aws-modules/main.tf
@@ -154,6 +154,26 @@ module "ecs" {
 	}
 }
 
+# * Step 6 - Create our ECS Task Definition
+data "aws_iam_role" "ecs_task_execution_role" { name = "ecsTaskExecutionRole" }
+resource "aws_ecs_task_definition" "this" {
+	container_definitions = jsonencode([{
+		environment: [
+			{ name = "MY_INPUT_ENV_VAR", value = "terraform-modified-env-var" }
+		],
+		essential = true,
+		image = resource.docker_registry_image.this.name,
+		name = local.container_name,
+		portMappings = [{ containerPort = local.container_port }],
+	}])
+	cpu = 256
+	execution_role_arn = data.aws_iam_role.ecs_task_execution_role.arn
+	family = "family-of-${local.example}-tasks"
+	memory = 512
+	network_mode = "awsvpc"
+	requires_compatibilities = ["FARGATE"]
+}
+
 # TODO: Update Step number
 # * Step XXX - See our application working.
 # * Output the URL of our Application Load Balancer so that we can connect to
