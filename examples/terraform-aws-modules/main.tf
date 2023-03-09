7d81dbb12cfdedae8e52c1953ad8017e1aca8cd5
1Mill <erik.ekberg@outlook.com>
2023-03-08 16:01:52 -0800

    Create ECS cluster

--- examples/terraform-aws-modules/main.tf
+++ examples/terraform-aws-modules/main.tf
@@ -129,6 +129,31 @@ module "alb" {
 	]
 }
 
+# * Step 5 - Create our ECS Cluster.
+module "ecs" {
+	source  = "terraform-aws-modules/ecs/aws"
+	version = "~> 4.1.3"
+
+	cluster_name = local.example
+
+	# * Allocate 20% capacity to FARGATE and then split
+	# * the remaining 80% capacity 50/50 between FARGATE
+	# * and FARGATE_SPOT.
+	fargate_capacity_providers = {
+		FARGATE = {
+			default_capacity_provider_strategy = {
+				base   = 20
+				weight = 50
+			}
+		}
+		FARGATE_SPOT = {
+			default_capacity_provider_strategy = {
+				weight = 50
+			}
+		}
+	}
+}
+
 # TODO: Update Step number
 # * Step XXX - See our application working.
 # * Output the URL of our Application Load Balancer so that we can connect to
