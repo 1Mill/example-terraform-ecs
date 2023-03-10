79f406e23a4eeca159bb6669dea85fc0a600a884
Nimit Shah <nimitzshah@gmail.com>
2023-03-10 14:13:23 -0500

    Add autoscaling

--- examples/aws-resources/main.tf
+++ examples/aws-resources/main.tf
@@ -283,6 +283,32 @@ resource "aws_ecs_service" "this" {
 	}
 }
 
+resource "aws_appautoscaling_target" "ecs_target" {
+	max_capacity       = 4
+	min_capacity       = 1
+	resource_id        = "service/${resource.aws_ecs_cluster.this.name}/${resource.aws_ecs_service.this.name}"
+	scalable_dimension = "ecs:service:DesiredCount"
+	service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "ecs_policy" {
+	name               = "scale-up-policy"
+	policy_type        = "TargetTrackingScaling"
+	resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+	scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+	service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+	target_tracking_scaling_policy_configuration {
+		target_value = 70
+		scale_in_cooldown = 300
+		scale_out_cooldown = 100
+
+		predefined_metric_specification {
+			predefined_metric_type = "ECSServiceAverageCPUUtilization"
+		}
+	}
+}
+
 # * Step 8 - See our application working.
 # * Output the URL of our Application Load Balancer so that we can connect to
 # * our application running inside  ECS once it is up and running.
